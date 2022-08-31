d8869d59144e9b81f5b156e137aa5db0bab2fc04
dylanrios25 <111157831+dylanrios25@users.noreply.github.com>
2022-08-31 17:58:13 -0600

    Add files via upload

---
+++ estructuradecontrol/condicionales.java
@@ -0,0 +1,35 @@
+package estructuradecontrol;
+
+public class condicionales {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//condiconal if
+		
+		byte edad= 21;
+		String nombre ="tony";
+		boolean gameover=true;
+			
+		if(gameover) {
+		  // se ejecute esto si la expresion es verdadera
+			
+			 System.out.println(nombre+ "tiene" + edad + "años de edad.");
+	}
+		//condiconal if else(double)
+		gameover=false;
+			String avenger="Ironman";
+
+         	if(gameover) {
+			System.out.println("fin del juego");	
+		}else {
+			System.out.println("fin del programa");
+		}
+			//Condiconal if else if(anidad)
+		
+        	
+         	
+         	
+		System.out.println("fin del programa");
+	}
+}
---
+++ estructuradecontrol/registro.java
@@ -0,0 +1,19 @@
+package estructuradecontrol;
+
+public class registro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		String genero,hombre;
+		byte edad =21;
+		boolean portarunarma =true;
+		
+	if(edad>0 && edad<25);
+	System.out.println("diga su edad");
+	
+		
+	}
+
+}
